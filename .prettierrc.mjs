e30d4854047b4c2a808649406f1f843d274c473e
kasuie <chenkauie@gmail.com>
2024-04-26 11:59:16 +0800

    init: 🎉init

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+/*
+ * @Author: kasuie
+ * @Date: 2024-04-26 11:57:32
+ * @LastEditors: kasuie
+ * @LastEditTime: 2024-04-26 11:57:45
+ * @Description: 
+ */
+import { factory } from "@kasuie/prettier";
+
+export default factory({ importSort: false, tailwindcss: true });
