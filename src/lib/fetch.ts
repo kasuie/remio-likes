a2bd17354a5b12c7fe86bb8eca61a9b31e994189
kasuie <chenkauie@gmail.com>
2024-12-27 14:16:45 +0800

    refactor: 🦄@kasuie/http

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -5,88 +5,57 @@
  * @LastEditTime: 2024-04-26 18:02:15
  * @Description:
  */
-import { isClientSide } from "./env";
-import message from "@/components/message/index";
+import {
+  fetch as Ifetch,
+  type RequestUrl,
+  type RequestOptions,
+} from "@kasuie/http";
+
+interface ResponseData {
+  data: any;
+  success: boolean;
+  message: string;
+}
+
 class API {
-  static async request(url: RequestInfo | URL, options = {}) {
-    return new Promise(async (resolve, reject) => {
-      const response = await fetch(url, {
-        headers: {
-          "Accept-Language":
-            "zh-CN,zh-CN;q=0.9,zh;q=0.8,en-US;q=0.7,en,en-CN;q=0.6",
-        },
-        ...options,
-      });
-
-      const data: any = await response.json();
-
-      if (response.ok) {
-        resolve(data);
-      } else {
-        isClientSide && message.error(`请求失败，错误码：${response.status}`);
-        reject(`${data?.message || "Request failed"}`);
-      }
-    });
+  static async get(
+    url: RequestUrl,
+    data?: unknown,
+    options?: Partial<RequestOptions>
+  ) {
+    return (await Ifetch.get(url, data, options)) as ResponseData;
   }
 
-  static get(url: RequestInfo | URL, params: any = {}, options = {}) {
-    params = new URLSearchParams(params).toString();
-    url = params ? `${url}?${params}` : url;
-    return API.request(url, {
-      method: "GET",
-      ...options,
-    });
+  static async post(
+    url: RequestUrl,
+    data?: unknown,
+    options?: Partial<RequestOptions>
+  ) {
+    return (await Ifetch.post(url, data, options)) as ResponseData;
   }
 
-  static post(url: RequestInfo | URL, body: any, options = {}) {
-    return API.request(url, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      ...options,
-    });
+  static async delete(
+    url: RequestUrl,
+    data?: unknown,
+    options?: Partial<RequestOptions>
+  ) {
+    return (await Ifetch.delete(url, data, options)) as ResponseData;
   }
 
-  static delete(url: RequestInfo | URL, body: any, options = {}) {
-    return API.request(url, {
-      method: "DELETE",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      ...options,
-    });
+  static async patch(
+    url: RequestUrl,
+    data?: unknown,
+    options?: Partial<RequestOptions>
+  ) {
+    return (await Ifetch.patch(url, data, options)) as ResponseData;
   }
 
-  static postQs(url: RequestInfo | URL, body?: any, options = {}) {
-    return API.request(url, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      ...options,
-    });
-  }
-
-  static form(url: RequestInfo | URL, body?: any, options = {}) {
-    return API.request(url, {
-      method: "POST",
-      body: body,
-      ...options,
-    });
-  }
-
-  //请求拦截
-  static async interceptRequest(url: any, options = {}) {
-    return { url, options };
-  }
-
-  //响应拦截
-  static async interceptResponse(response: any) {
-    return response;
+  static async put(
+    url: RequestUrl,
+    data?: unknown,
+    options?: Partial<RequestOptions>
+  ) {
+    return (await Ifetch.put(url, data, options)) as ResponseData;
   }
 }
 
